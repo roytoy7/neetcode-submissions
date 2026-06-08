1e2b8e9049ab76dfcca413a70350775ab4174b29
Amrita Roy <amrita.roy7417@gmail.com>
2026-06-08 19:59:47 -0400

    Add: find-duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-7.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i])-1] < 0) return Math.abs(nums[i]);
+            else {
+                nums[Math.abs(nums[i])-1] *= -1;
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
