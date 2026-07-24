676f05522badb3ad737033b5bed36bc023541fe6
Amrita Roy <amrita.roy7417@gmail.com>
2026-07-23 20:13:57 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        gen(res, new StringBuilder(), n, n);
+        return res;
+    }
+    private void gen(List<String> res, StringBuilder sb, int l, int r) {
+        if (l==0&&r==0) {
+            res.addLast(sb.toString()); 
+            return;
+        }
+
+        if (l>0) {
+            sb.append("(");
+            gen(res, sb, l-1, r);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        if (r>l) {
+            sb.append(")");
+            gen(res, sb, l, r-1);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        
+    }
+}
