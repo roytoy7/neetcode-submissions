72ca8c7c3efb241ef4053c957656a25cb82b6a62
Amrita Roy <amrita.roy7417@gmail.com>
2026-09-04 22:37:00 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, 0);
+        dp(0, memo, nums);
+
+        return memo[0];
+    }
+    private int dp(int i, int[] memo, int[] nums) {
+        if (i>=nums.length) return 0;
+        if (memo[i]!=0) return memo[i];
+
+        int rob_cur = nums[i] + dp(i+2, memo, nums);
+        int skip_cur = dp(i+1, memo, nums);
+
+        memo[i] = Math.max(rob_cur, skip_cur);
+        
+        return memo[i];
+    }
+}
